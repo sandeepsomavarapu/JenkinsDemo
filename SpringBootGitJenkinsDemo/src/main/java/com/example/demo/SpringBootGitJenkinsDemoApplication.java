2ee73e34237569d43630a0accec227f6b5675727
sandeepsomavarapu535@gmail.com <sandeepsomavarapu535@gmail.com>
2021-08-24 13:54:00 +0530

    Jenkins Demo Commit

---
+++ SpringBootGitJenkinsDemo/src/main/java/com/example/demo/SpringBootGitJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitJenkinsDemoApplication.class, args);
+	}
+
+}
